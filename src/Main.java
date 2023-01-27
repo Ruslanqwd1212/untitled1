b0f40d60def1c86592e482ed681c57aad7a7add5
Ruslanqwd1212 <116277908+Ruslanqwd1212@users.noreply.github.com>
2023-01-27 17:00:45 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+                Task task = new Task();
+                task.linked();
+
+            }
+        }
+
---
+++ src/Random.java
@@ -0,0 +1,9 @@
+public class Random {
+    public int random() {
+        return (int) (Math.random() * 100);
+    }
+    public int randomHunT() {
+        return (int) (Math.random() * 100000);
+    }
+}
+
